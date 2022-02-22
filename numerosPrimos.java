a2a52297f5120ad04f699479b2e755a757336ce8
Jose Gordon Motino <josegordonm86@gmail.com>
2022-02-22 09:29:14 +0000

    prueba

--- numerosPrimos.java
+++ numerosPrimos.java
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
+
 // Esta es mi funcion
-=======
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-// Esta es mi funcion, estoy en la rama experimentafuncion
->>>>>>> experimentafuncion
-=======
-=======
-
->>>>>>> experimentafuncion
-// Esta es mi funcion, estoy en la rama experimentafuncion.
->>>>>>> experimentafuncion
 public static boolean esPrimo(int numero){
   int contador = 2;
   boolean primo=true;
